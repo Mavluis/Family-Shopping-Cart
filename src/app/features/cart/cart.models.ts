1f72218f038894a2e153f6ea7e8d61a17303b5cb
Luis <mavluis@gmail.com>
2019-06-14 16:09:02 +0200

    models add

--- src/app/features/cart/cart.models.ts
+++ src/app/features/cart/cart.models.ts
@@ -14,3 +14,54 @@ export interface Post {
     createdAt: any;
     id: string[];
 }
+
+export interface ShoppingCartUser {
+    uuid: string;
+    fullName: string;
+    email: string;
+    idproducts: Idproducts;
+}
+
+export interface Idproducts {
+    beer: boolean;
+    galletas: boolean;
+    cereals: boolean;
+    chips: boolean;
+    fruit: boolean;
+    milk: boolean;
+    potatoes: boolean;
+    tomatobrick: boolean;
+    water: boolean;
+    yogourt: boolean;
+    butter: boolean;
+    coldcuts: boolean;
+    eggs: boolean;
+    eggplant: boolean;
+    macaroni: boolean;
+    mushrooms: boolean;
+    olives: boolean;
+    sausages: boolean;
+    spaghetti: boolean;
+    squash: boolean;
+    apples: boolean;
+    celery: boolean;
+    cleaningsupplies: boolean;
+    melon: boolean;
+    onions: boolean;
+    parchmentpaper: boolean;
+    pears: boolean;
+    pizza: boolean;
+    strawberries: boolean;
+    zucchini: boolean;
+    bananas: boolean;
+    bottledwater: boolean;
+    cherry: boolean;
+    coffee: boolean;
+    deodorant: boolean;
+    glasscleaner: boolean;
+    orangejuice: boolean;
+    redwine: boolean;
+    toiletpaper: boolean;
+    watermelon: boolean;
+    notes: string;
+}
