5b65d2e4929d9feb60cf264641d98ff64cbdb7fb
MavLuis <mavluis@gmail.com>
2020-03-29 18:59:42 +0200

    change idioma

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -13,13 +13,17 @@ export class NotificationService {
     this.route.queryParams.subscribe(params => {
       if (params.notification === 'account-activated') {
         this.modalService.open(
-          'Account Activated!!',
-          'Please, log in and enter your Shopping Cart!!!'
+          // 'Account Activated!!',
+          'Cuenta Activada!!',
+          // 'Please, log in and enter your Shopping Cart!!!'
+          'Por favor, haz login y entra en tu cuenta Shopping Cart!!!'
         );
       } else if (params.notification === 'account-not-activated') {
         this.modalService.open(
-          'Account Not Activated',
-          'Sorry we cannot activate your account'
+          // 'Account Not Activated',
+          'Cuenta NO Activada!!!',
+          // 'Sorry we cannot activate your account'
+          'Lo siento, no podemos activar tu cuenta, ponte en contacto con support@familyshopcart.com, gracias.'
         );
       }
     });
